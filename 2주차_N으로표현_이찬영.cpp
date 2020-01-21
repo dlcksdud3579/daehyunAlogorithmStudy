389f860bf49b811a6767c79c8c5a25a65dce9ca4
ay3579 <dlcksdud3579@naver.com>
2020-01-21 18:09:12 +0900

    57점 으로 수정

--- 2주차_N으로표현_이찬영.cpp
+++ 2주차_N으로표현_이찬영.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 map<int, int> m;
+vector<int>* NList[9];
 
 void insertMap(int key,int value)
 {
@@ -15,18 +16,22 @@ void insertMap(int key,int value)
     if (preValue == 0 || preValue > value)
         m[key] = value;
 }
+void pushNList(int index,int value)
+{
+    if(value > -5000  && value<50000 )
+        NList[index]->push_back(value);
 
-
+}
 
 
 int solution(int N, int number) {
     int answer = -1;
-    vector<int> *NList[9];
+    
 
     for(int i = 1;i<9;i++)
         NList[i] = new vector<int>();
 
-    NList[1]->push_back(N);
+    pushNList(1, N); //NList[1]->push_back(N);
     insertMap(N, 1); // depth 1일 경우  
     
 
@@ -39,37 +44,45 @@ int solution(int N, int number) {
         {
             NNN += N* pow(10, j);
         }
-        NList[i]->push_back(NNN);
+        pushNList(i, NNN);//NList[i]->push_back(NNN);
        insertMap(NNN, i);
         //~ N...N 일 경우
 
         for (vector<int>::iterator iter = NList[i-1]->begin(); iter != NList[i - 1]->end(); ++iter) {
             int preN = *iter;  // 이전 노드 N
             int empty = 0;  //연산 값 임시 저장 
+            for (int j = 0; i + j <= 8; j++)
+            {
+                int emptyN = 0;
+                for (int k = 0; k <= j; k++)
+                {
+                    emptyN += N * pow(10, k);
+                }
+              
+                // *연산 
+                empty = preN * emptyN;
+                insertMap(empty, i);
+                pushNList(i, empty);//NList[i]->push_back(empty);
+                // ~ *연산
 
-            // *연산 
-            empty = preN * N;
-            insertMap(empty, i);
-            NList[i]->push_back(empty);
-            // ~ *연산
+                // /연산 
+                empty = preN / emptyN;
+                insertMap(empty, i);
+                pushNList(i, empty);//NList[i]->push_back(empty);
+                // ~ /연산
 
-            // /연산 
-            empty = preN / N;
-            insertMap(empty, i);
-            NList[i]->push_back(empty);
-            // ~ /연산
+                //  + 연산 
+                empty = preN + emptyN;
+                insertMap(empty, i);
+                pushNList(i, empty);//NList[i]->push_back(empty);
+                //  +연산
 
-            //  + 연산 
-            empty = preN + N;
-            insertMap(empty, i);
-            NList[i]->push_back(empty);
-            //  +연산
-
-            //  -연산 
-            empty = preN - N;
-            insertMap(empty, i);
-            NList[i]->push_back(empty);
-            // ~  -연산
+                //  -연산 
+                empty = preN - emptyN;
+                insertMap(empty, i);
+                pushNList(i, empty);//NList[i]->push_back(empty);
+                // ~  -연산
+            }
         }
     }
     
@@ -82,5 +95,5 @@ int solution(int N, int number) {
 
 int main()
 {
-    solution(5, 12);
+    cout << solution(5, 3025);
 }
